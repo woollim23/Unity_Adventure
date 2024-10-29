c8dcb2e8e02885abb281870e04d182f771a0900e
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-29 21:57:20 +0900

    feat(player) : 스테미나 사용하기

--- Unity_Adventure/Assets/Scripts/Item/EquipTool.cs
+++ Unity_Adventure/Assets/Scripts/Item/EquipTool.cs
@@ -36,4 +36,23 @@ public class EquipTool : Equip
     {
         attacking = false;
     }
+
+    public void OnHit()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, attackDistance))
+        {
+            if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
+            {
+                resource.Gather(hit.point, hit.normal);
+            }
+
+            //if (doesDealDamage && hit.collider.TryGetComponent(out IDamagable damagable))
+            //{
+            //    damagable.TakePhysicalDamage(damage);
+            //}
+        }
+    }
 }
