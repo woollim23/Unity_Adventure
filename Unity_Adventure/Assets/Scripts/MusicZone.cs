41f33dd1a0cbe7e4907ce2d288b90b0db92d48d8
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-30 12:39:37 +0900

    feat(Music zone) : 적(곰)에 뮤직존을 부착하여 근접하면 BGM 소리가 점점 커지도록 함

--- Unity_Adventure/Assets/Scripts/MusicZone.cs
+++ Unity_Adventure/Assets/Scripts/MusicZone.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource audioSource;
+    public float fadeTime;
+    public float maxVolume;
+    private float targetVolume;
+
+    private void Start()
     {
-        
+        targetVolume = 0.0f;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = targetVolume;
+        audioSource.Play();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Mathf.Approximately : 근사값이면 같은 값으로 인식
+        // 근사값이 아닐때
+        if (!Mathf.Approximately(audioSource.volume, targetVolume))
+        {
+            // Mathf.MoveTowards : 점진적으로 늘어남
+            audioSource.volume = Mathf.MoveTowards(audioSource.volume, targetVolume, (maxVolume / fadeTime) * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            targetVolume = maxVolume;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            targetVolume = 0.0f;
     }
 }
--- Unity_Adventure/Assets/Scripts/Player/FootSteps.cs
+++ Unity_Adventure/Assets/Scripts/Player/FootSteps.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FootSteps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] footstepClips;
+    private AudioSource audioSource;
+    private Rigidbody _rigidbody;
+    public float footstepThreshold; // 리지드 바디에서 어느정도 움직였을때의 기준점
+    public float footstepRate; // 간격
+    private float footStepTime;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Mathf.Abs(_rigidbody.velocity.y) < 0.1f)
+        {
+            // magnitude 크기값
+            if (_rigidbody.velocity.magnitude > footstepThreshold)
+            {
+                if (Time.time - footStepTime > footstepRate)
+                {
+                    footStepTime = Time.time;
+                    audioSource.PlayOneShot(footstepClips[Random.Range(0, footstepClips.Length)]);
+                }
+            }
+        }
     }
 }
